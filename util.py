e88767bbf4fe1cf4aa1d414fd41fbed6153508a2
bbjy <1910511934@qq.com>
2023-02-18 00:07:12 +0800

    Update util.py

--- util.py
+++ util.py
@@ -8,7 +8,6 @@ import os
 import pandas as pd
 import copy
 import random
-#将target序列的字母编码为数字
 seq_voc = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
 seq_dict = {v:(i+1) for i,v in enumerate(seq_voc)}
 seq_dict_len = len(seq_dict)
@@ -35,16 +34,9 @@ def generate_adj(sp_H):
 	HT = sp_H.T
 	adj = HT.dot(sp_H)
 	shape = adj.shape
-	# scaling
-	# print('adj: ',adj[0,:].todense())
-	# col_max = np.array(adj.max(0).todense()).flatten()
-	# print('col_max', len(col_max))
 	row_index, col_index = adj.nonzero()
 	val = np.ones(len(col_index))
 	adj = csr_matrix((val,(row_index,col_index)), shape=shape) 
-	# adj.data /= col_max[col_index]
-	# print('adj.shape: ',adj.shape)
-	# print(adj[0,:].todense())
 	return adj
 
 def read_data_set(data_dir, test_ratio, isfeaturize=False):
@@ -58,7 +50,7 @@ def read_data_set(data_dir, test_ratio, isfeaturize=False):
 	data = np.load(os.path.join(data_dir,TRAIN_FILE))
 	print('train_data.shape: ', data['train_data'].shape)
 	nums_type_all = data['nums_type_all']
-	train_data = Data(data['train_data'], all_events, data['num_dis_train']) # DataSet 里加入SMILES和protein sequence
+	train_data = Data(data['train_data'], all_events, data['num_dis_train']) 
 	del data
 
 	data = np.load(os.path.join(data_dir, VALID_FILE))
@@ -76,14 +68,9 @@ def read_data_set(data_dir, test_ratio, isfeaturize=False):
 	print('HT.shape: ',HT.shape)
 
 	if isfeaturize:	
-		# 获取drug的ECPF fingerprint vector
 		print('read drug SMILES ...')
 		df = pd.read_csv(os.path.join(data_dir,'chem_file.csv'),header=0,sep='\t')
-		# print(df.columns.values)
 		compound_iso_smiles = list(df['SMILES'])
-		# compound_iso_smiles = set(compound_iso_smiles)
-		# atomFeaturizer = graph_features.WeaveFeaturizer()
-		
 		featurizer = dc.feat.CircularFingerprint(size=1024)
 		ecfp = featurizer.featurize(compound_iso_smiles)
 		print('ecfp.shape: ',ecfp.shape)
@@ -95,18 +82,15 @@ def read_data_set(data_dir, test_ratio, isfeaturize=False):
 				arr = np.random.randint(0,2,size=[1024]) 
 			ecfp_arr = np.vstack((ecfp_arr,arr))
 		non_zero_row, non_zero_column = np.where(ecfp_arr>0)
-		ecfp_arr[np.where(ecfp_arr>0)] = non_zero_column + 1 #加1是为了让含1个数不同的multi-hot特征向量对齐，即多编码一个0
+		ecfp_arr[np.where(ecfp_arr>0)] = non_zero_column + 1 
 		print('ecfp_arr.shape: ' ,ecfp_arr.shape)
 		
-		# 编码target的序列化表示
 		print('read target sequence...')
 		df = pd.read_csv(os.path.join(data_dir,'gene_file.csv'),header=0,sep='\t')
 		target_sequence = list(df['sequence'])
 		XT = [seq_cat(t) for t in target_sequence]
 		XT_arr = np.array(XT)
 		print('type(XT_arr): ', type(XT_arr))
-		# 编码disease
-		# todo
 		dataset['ecfp'] = ecfp_arr
 		dataset['XT'] = XT_arr #vector of target
 
@@ -115,16 +99,8 @@ def read_data_set(data_dir, test_ratio, isfeaturize=False):
 	dataset['train_data'] = train_data
 	dataset['valid_data'] = valid_data
 	dataset['test_data'] = test_data
-	# dataset['num_drug'] = num_drug
-	# dataset['num_target'] = num_target
-	# dataset['num_disease'] = num_disease
 	dataset['nums_type_all'] = nums_type_all
-	# dataset['ecfp_input'] = np.array(ecfp_input)
-	# dataset['ecfp_offset'] = np.array(ecfp_offset)
-	# dataset['seq_input'] = seq_input
-	# dataset['seq_offset'] = seq_offset
 	dataset['HT'] = HT
-	# dataset['node_adj'] = node_adj
 	return dataset
 
 class Data():
@@ -133,8 +109,8 @@ class Data():
 	'''
 	def __init__(self, data, all_events, num_disease, **kwargs):
 		'''
-			data 里存放的是train/valid/test (event_id, dis_id)
-			all_events 里存放的是所有的events，用于选择negative sample时判断
+			data: train/valid/test (event_id, dis_id)
+			all_events: all events
 		'''
 		self.data = data
 		self.all_events = all_events
@@ -171,7 +147,6 @@ class Data():
 			if len(neg_data) > 0:
 				neg_data = np.array(neg_data)
 				batch_data = np.vstack((self.data[start:end], neg_data))
-				# print('batch_data.shape: ',batch_data.shape)
 				nums_batch = len(batch_data)
 				labels = np.zeros(nums_batch)
 				labels[0:end-start] = 1
@@ -186,10 +161,10 @@ class Data():
 
 def early_stopping(cur_auc, best_auc, stopping_step, flag_step):
 	'''
-		cur_auc: 当前auc值
-		best_auc: 目前为止最好的auc值
-		stopping_step：从上一次出现最好auc值开始到本轮，auc没有提升的步数
-		flag_step：如果auc连续flag_step步都没有提升，则执行early_stopping
+		cur_auc: current auc
+		best_auc: the best auc
+		stopping_step
+		flag_step: early stopping at 'flag' step
 	'''
 	if cur_auc >= best_auc:
 		stopping_step = 0
